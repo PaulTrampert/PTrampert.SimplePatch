1eaf345ab40a49a475ea6957c47cee8ee6b37366
Paul Trampert <paul.trampert@ptrampert.com>
2025-08-07 23:18:43 -0400

    Add doc comments (PATCH)

--- PTrampert.Optionals/OptionalValidationAttribute.cs
+++ PTrampert.Optionals/OptionalValidationAttribute.cs
@@ -2,9 +2,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PTrampert.Optionals;
 
+/// <summary>
+/// Validation attribute for properties of type <see cref="IOptional"/>.
+/// This attribute is not intended to be used directly, but is applied by the <see cref="PatchClassBuilder"/>
+/// to properties of patch objects that implement <see cref="IPatchObjectFor{T}"/>.
+/// It runs the validators of the corresponding property in the original type, if the optional has a value.
+/// </summary>
+/// <param name="innerValidatorType">The validator type on the original class's corresponding property.</param>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
 public class OptionalValidationAttribute(Type innerValidatorType) : ValidationAttribute
 {
+    /// <inheritdoc />
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
         if (value is not IOptional optional)
--- PTrampert.Optionals/PatchJsonConverterFactory.cs
+++ PTrampert.Optionals/PatchJsonConverterFactory.cs
@@ -4,16 +4,29 @@ using System.Text.Json.Serialization;
 
 namespace PTrampert.Optionals;
 
+/// <summary>
+/// JsonConverterFactory for creating converters for patch objects.
+/// This factory generates a converter for types implementing <see cref="IPatchObjectFor{T}"/>.
+/// The converter serializes and deserializes the patch object, allowing it to be used
+/// with System.Text.Json serialization and deserialization.
+/// The patch object is a type that can apply changes to an instance of type T, where T is typically a POCO or record type.
+/// </summary>
 public class PatchJsonConverterFactory : JsonConverterFactory
 {
     private readonly PatchClassBuilder _patchClassBuilder = new();
 
+    /// <summary>
+    /// Returns true if the specified type is a generic type of <see cref="IPatchObjectFor{T}"/>.
+    /// </summary>
+    /// <param name="typeToConvert">The type to convert.</param>
+    /// <returns>True if typeToConvert is <see cref="IPatchObjectFor{T}"/></returns>
     public override bool CanConvert(Type typeToConvert)
     {
         return typeToConvert.IsGenericType &&
                typeToConvert.GetGenericTypeDefinition() == typeof(IPatchObjectFor<>);
     }
 
+    /// <inheritdoc />
     public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
     {
         var baseObjectType = typeToConvert.GetGenericArguments()[0];
